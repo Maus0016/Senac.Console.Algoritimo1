32de86183bc05f76ed799d492becdc7142a84d09
Daniel Maus <hakumennomodo@gmail.com>
2025-03-25 21:50:28 -0300

    LEITURA DE VARIAVEIS

--- Algoritomo2/Algoritomo2/Program.cs
+++ Algoritomo2/Algoritomo2/Program.cs
@@ -4,3 +4,15 @@ decimal preco = 20M;
 char letra = 'a';
 var nomeCompleto = "Rafael Vieira Suarez";
 var altura = 180;
+
+Colsole.WriteLine("Digite seu nome");
+nomeCompleto = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine(""Bem vindo " + nomeCompleto);
+Console.Write("Digite sua idade: ");
+var linha - Console.ReadLine();
+idade = int.Parse(linha);
+Console.WriteLine("Idade informada " + idade);
+if (idade < 18)
+    Console.WriteLine("você não pode estar aqui!");
+Console.ReadKey();
