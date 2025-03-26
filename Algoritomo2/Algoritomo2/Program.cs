7b73bd779519e42e4075e55f56dd60e53960741d
Daniel Maus <hakumennomodo@gmail.com>
2025-03-25 21:56:00 -0300

    erros

--- Algoritomo2/Algoritomo2/Program.cs
+++ Algoritomo2/Algoritomo2/Program.cs
@@ -5,14 +5,15 @@ char letra = 'a';
 var nomeCompleto = "Rafael Vieira Suarez";
 var altura = 180;
 
-Colsole.WriteLine("Digite seu nome");
+Console.WriteLine("Digite seu nome");
 nomeCompleto = Console.ReadLine();
 Console.WriteLine();
-Console.WriteLine(""Bem vindo " + nomeCompleto);
+Console.WriteLine("Bem vindo " + nomeCompleto);
 Console.Write("Digite sua idade: ");
-var linha - Console.ReadLine();
+var linha = Console.ReadLine();
 idade = int.Parse(linha);
 Console.WriteLine("Idade informada " + idade);
 if (idade < 18)
     Console.WriteLine("você não pode estar aqui!");
+
 Console.ReadKey();
